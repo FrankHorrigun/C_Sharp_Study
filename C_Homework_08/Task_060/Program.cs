1e48f4cefa04180f9477d10c1073ec96982c8b8e
FrankHorrigun <FrankHorrigun@mail.ru>
2022-11-28 14:31:28 +0200

    8 домашка

--- C_Homework_08/Task_060/Program.cs
+++ C_Homework_08/Task_060/Program.cs
@@ -1,31 +1,80 @@
 ﻿//Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
 //Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 
-void CreateCubeArray()
+int Prompt(string message)
 {
-    int[,,] cube = new int[x, y, z];
-    for (int i = 0; cube.GetLength(0); i++)
+    Console.WriteLine(message);
+    int value;
+    while (!int.TryParse(Console.ReadLine(), out value) ^ value <= 0)
     {
-        for (int j = 0; cube.GetLength(1); j++)
-        {
-            for (int k = 0; cube.GetLength(2); k++)
-            {
+        Console.WriteLine("Введите число больше нуля ");
+        Console.WriteLine(message);
+    }
+    return value;
+}
 
+
+int[,,] CreateMatrix(int row, int col, int dep)
+{
+    int[,,] matrix = new int[row, col, dep];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = GetUniqueValue(matrix, i, j, k);
             }
+        }
+    }
+    return matrix;
+}
+
+int GetUniqueValue(int[,,] matrix, int i, int j, int k)
+{
+    int value = default;
+    bool exist = true;
+    while (exist)
+    {
+        bool _break = false;
+        Random rnd = new Random();
+        value = rnd.Next(10, 100);
+        for (int i1 = 0; i1 < matrix.GetLength(0); i1++)
+        {
+            if (_break) { break; }
+            for (int j1 = 0; j1 < matrix.GetLength(1); j1++)
+            {
+                if (_break) { break; }
+                for (int k1 = 0; k1 < matrix.GetLength(2); k1++)
+                {
+                    if (matrix[i1, j1, k1] == value) { _break = true; break; }
+                    if (i1 == i && j1 == j && k1 == k) { exist = false; }
+                }
+            }
+        }
+    }
+    return value;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k],1} ({i}, {j}, {k})|");
+            }
+            Console.WriteLine();
         }
     }
 }
 
-int UniqRandomGenerator(int x)
-{
-    Random rnd = new Random();
-    int a = rnd.Next(0, x * 10);
-    int[] uniqElemenets = new int[x*x*x];
-    do (int new = a) while
-    {
-        
-    }
 
+int row = Prompt("Введите высоту матрицы");
+int col = Prompt("Введите ширину матрицы");
+int depth = Prompt("Введите глубину матрицы");
 
-}
-
+int[,,] matrix3D = CreateMatrix(row, col, depth);
+PrintMatrix(matrix3D);
--- C_Homework_08/Task_062/Program.cs
+++ C_Homework_08/Task_062/Program.cs
@@ -1,27 +1,38 @@
-﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+﻿// Напишите программу, которая заполнит спирально массив n на n.
 
-int[,] CreateRandomMatrix()
+int Prompt(string message)
 {
-    int[,] randomArray = new int[4, 4];
-    randomArray[0, 0] = 1;
-    for (int i = 0; i < randomArray.GetLength(0); i++)
+    Console.WriteLine(message);
+    int value;
+    while (!int.TryParse(Console.ReadLine(), out value) ^ value <= 0)
     {
-        if (i != randomArray.Length-1)
-        {
-            for (int j = 1; j < randomArray.GetLength(0); j++)
-            {
-                randomArray[i, j] = randomArray[i, j - 1] + 1;
-            }
-        }
-        if( i == randomArray.Length-1)
-        {
-            for( int j = array.MaxLength; j++)
-            {
-                randomArray[i, j] = randomArray[i, j] + 1;
-            }
-        }
+        Console.WriteLine("Введите число больше нуля ");
+        Console.WriteLine(message);
     }
-    return randomArray;
+    return value;
+}
+
+
+int[,] CreateSpiralMatrix(int n)
+{
+    int[,] a = new int[n, n];
+
+    int i = 0, j = 0;
+
+    int value = 1;
+
+    while (n != 0)
+    {
+        int k = 0;
+        do { a[i, j++] = value++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) a[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) a[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) a[i--, j] = value++;
+
+        ++i; ++j; n = n < 2 ? 0 : n - 2;
+    }
+    return a;
+
 }
 
 void PrintArray(int[,] array)
@@ -30,11 +41,12 @@ void PrintArray(int[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write($"{array[i, j]} ");
+            Console.Write("{0,3}", array[i, j]);
         }
         Console.WriteLine();
     }
 }
 
-int[,] snakeMatrix = CreateRandomMatrix();
-PrintArray(snakeMatrix);
+int n = Prompt("Введите размер матрицы");
+int[,] spiralMatrix = CreateSpiralMatrix(n);
+PrintArray(spiralMatrix);
